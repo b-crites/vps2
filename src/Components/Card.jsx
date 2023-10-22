abf5d7730e42761b2fab7d37c092765652e843db
Brandon Crites <brandoncrites76@gmail.com>
2023-10-22 10:12:51 -0700

    package JSON

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -17,7 +17,7 @@ export default function Card({name, title, image, preview, content}){
         <div className="xl:w-1/4 w-2/3  bg-white rounded-lg shadow-lg overflow-hidden xl:flex xl:flex-col xl:justify-center xl:items-center">
             <div className="">
             <div>
-                <img className="object-center object-cover h-auto w-full" src= {image} alt={`${name}'s Photo`} />
+                <img className="object-center object-cover h-auto w-full" src={image} alt={`${name}'s Photo`} />
             </div>
             <div className="text-center py-8 sm:py-6">
                 <p className="text-xl text-gray-700 font-bold mb-2">{name}</p>
@@ -32,6 +32,6 @@ export default function Card({name, title, image, preview, content}){
 
         </div>
 
-
+ 
     );
 }
